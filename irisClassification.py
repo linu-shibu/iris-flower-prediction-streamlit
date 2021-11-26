c85ba26f1c3de4289d21b1bebdf6e7b8beb601e1
Linu <linushibu.ls@gmail.com>
2021-11-26 20:20:42 +0530

    first commit

---
+++ irisClassification.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+
+st.write("""
+# Iris Flower prediction app
+
+This app predicts the **iris flower** type from four user input features of the flower
+""")
+
+st.sidebar.header('User Input Parameters')
+
+
+def user_input_features():
+    sepal_length = st.sidebar.slider('Sepal Length', 4.3, 7.9, 5.4)
+    sepal_width = st.sidebar.slider('Sepal Width', 2.0, 4.4, 3.4)
+    petal_length = st.sidebar.slider('Petal Length', 1.0, 6.9, 1.3)
+    petal_width = st.sidebar.slider('Petal Width', 0.1, 2.5, 0.2)
+    data = {'sepal_length': sepal_length,
+            'sepal_width': sepal_width,
+            'petal_length': petal_length,
+            'petal_width': petal_width}
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df)
+
+iris = datasets.load_iris()
+X = iris.data
+Y = iris.target
+
+rfc = RandomForestClassifier()
+rfc.fit(X, Y)
+
+prediction = rfc.predict(df)
+prediction_prob = rfc.predict_proba(df)
+
+st.subheader('Class labels and their corresponding index numbers')
+st.write(iris.target_names)
+
+st.subheader('Prediction')
+st.write(iris.target_names[prediction])
+
+st.subheader('Prediction probability')
+st.write(prediction_prob)
